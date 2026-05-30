8f1b8024b64ad65b3bc86170ad9ef2e002ebd2ff
jayesh-cws <jayesh@creativewebsolution.in>
2026-05-30 16:56:55 +0530

    fix prisma global type

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-export { prisma }
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
